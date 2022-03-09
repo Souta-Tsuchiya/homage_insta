ac9b3e774cacd921ee45993f40df8bd1cc8ef687
Souta-Tsuchiya <legacy_6piston@yahoo.co.jp>
2022-03-10 01:50:55 +0900

    多言語化

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
-import 'dart:math';
-
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:homage_insta/generated/l10n.dart';
 import 'package:homage_insta/view/home_screen.dart';
 
 void main() => runApp(MyApp());
@@ -20,6 +20,13 @@ class MyApp extends StatelessWidget {
           style: ElevatedButton.styleFrom(primary: Colors.white)
         ),
       ),
+      localizationsDelegates: [
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: S.delegate.supportedLocales,
       home: HomeScreen(),
     );
   }
